92845b34b999f60a2528bedd4ab5fd67d79ec90b
LoopFruits <Kensworldc@gmail.com>
2022-05-07 19:29:23 -0400

    commit tree 6d7e9d29725719ede15870e88b9101f90600bc26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
-    def show
-      user = User.find_by(id: session[:user_id])
-      if user
-        render json: user
-      else
-        render json: { error: "Not authorized" }, status: :unauthorized
-      end
+
+  #finds a user in the database using the user id from the session hash and returns the user as json
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
     end
   end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # resources :sessions, only: [:create, :destroy]
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
+  #returns a user when that user is logged in
   get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
